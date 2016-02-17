33e6b49c8105d7fb052fb42cb4549d40e165b0dd
Cebrail Erdogan <cebonet@msn.com>
2016-02-17 19:08:34 +0100

    first commit

---
+++ batteryNotify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BATPATH=/sys/class/power_supply/BAT0
+BAT_FULL=$BATPATH/charge_full
+BAT_NOW=$BATPATH/charge_now
+BAT_STATUS=$BATPATH/status
+bf=$(cat $BAT_FULL)
+bn=$(cat $BAT_NOW)
+bs=$(cat $BAT_STATUS)
+if [ $(( 100 * $bn / $bf )) -lt 15 -a "$bs" != "Charging" ]
+then
+    i3-nagbar -t error -m 'LOW BATTERY'
+fi
+
